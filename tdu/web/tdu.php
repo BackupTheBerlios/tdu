80da4d0db7b7ddd3776666fa1d0d2d34b1d338fe
hede <hede>
2007-03-08 18:19:17 +0000

    mein erstes commit... Lizenzen ergaenzt

--- tdu/web/tdu.php
+++ tdu/web/tdu.php
@@ -1,4 +1,21 @@
 <?php
+/* tdu - TreeDiskUsage -
+   copyright michael.heide@student.uni-siegen.de
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+
   //TODO: kontrolle, ob mittlerweiler neuer scan. STARTZEIT!
 
   //with this all configs and additional functions will be loaded
